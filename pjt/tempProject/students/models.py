6d77a601c95c28bcb3173fa57b1494f2797554de
rabbitkingez <rabbitkingez@gmail.com>
2020-08-12 16:19:52 +0900

    tempProject
    temperary django project

---
+++ pjt/tempProject/students/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    studentName = models.CharField(max_length=100)
+    studentMajor = models.CharField(max_length=100)
+    studentAge = models.IntegerField(default=0)
+    studentGrade = models.IntegerField(default=0)
+    studentGender = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.studentName
