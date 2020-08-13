3e0a677616caba293f00c28e97af170a1d300efa
rabbitkingez <rabbitkingez@gmail.com>
2020-08-13 09:52:49 +0900

    admin 모델 import 주석 추가

--- pjt/tempProject/students/admin.py
+++ pjt/tempProject/students/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from students.models import Student
+from students.models import Student # 모델 import
 # Register your models here.
 
 admin.site.register(Student) # 모델을 등록
